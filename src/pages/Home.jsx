436903d8d4c432d3d15424b8aa537b979419b1cf
miunmnn <esteban.principe@utec.edu.pe>
2022-02-28 19:58:04 -0500

    Added pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,42 @@
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+import Spectogram from "../img/spectogram.png"
+import RVIZ from "../img/rviz.PNG"
+import Histograma from "../img/histograma.jpg"
 
 
-
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'black' ? 'black' : 'black',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const Home = () =>{
   return (
-    <div>Software</div>
+    <div
+    // style={{ backgroundColor: "black", height:'100%'  }}
+    
+    >
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
+        <Item><img src={Histograma} alt="histograma"/></Item>
+      </Grid>
+      <Grid item xs={6}>
+        <Item><img src={Histograma} alt="" /></Item>
+      </Grid>
+      <Grid item xs={6}>
+        <Item><img src={RVIZ} alt="rviz"/></Item>
+      </Grid>
+      <Grid item xs={6}>
+        <Item><img src={Spectogram} alt="spectograma"/></Item>
+      </Grid>
+    </Grid>  
+    </div>
   )
 }
 
