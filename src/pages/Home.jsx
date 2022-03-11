be3c71566e9e38b8a18b8685a02d1ec454decd1d
renatoseb <renatorodriguezllanos@gmail.com>
2022-03-11 16:43:58 -0500

    adding titles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,21 +2,30 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack'
-import Button from '@mui/material/Button'
+import Button from '@mui/material/Button';
+import Chip from '@mui/material/Chip';
 
 import Spectogram from "../img/spectogram.png"
 import RVIZ from "../img/rviz.PNG"
 import Histograma from "../img/histograma.jpg"
 
 
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: 'white',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+
+// }));
+
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: 'white',
   ...theme.typography.body2,
-  padding: theme.spacing(1),
   textAlign: 'center',
   color: theme.palette.text.secondary,
-
+  // height: 60,
+  lineHeight: '60px',
+  fontSize: '26px',
 }));
 
 
@@ -66,6 +75,10 @@ const boxStyleRow = (height) => {
   })
 }
 
+const TitleStyle = {
+
+}
+
 const Home = () => {
   return (
     <Box sx={{ height: '850px', width: '100%', display: 'flex' }}>
@@ -75,9 +88,20 @@ const Home = () => {
       </Box>
       <Box sx={boxStyleCols("100%", "50%")}>
         <Box sx={boxStyleRow("40%")}>
-          <Grid container justifyContent="flexStart" xs={12}>
-            <StyledButton variant='contained' color='success'>ON</StyledButton>
-            <StyledButton variant='contained' color='error'>OFF</StyledButton>
+          <Grid container justifyContent="flexStart">
+            <Grid item xs={12}>
+              <Item elevation={8}>
+                Valores
+              </Item>
+            </Grid>
+            <Grid item xs={12}>
+              <Chip> C02</Chip>
+            </Grid>
+            <Grid item xs={12}>
+              <StyledButton variant='contained' color='success'>ON</StyledButton>
+              <StyledButton variant='contained' color='error'>OFF</StyledButton>
+            </Grid>
+
           </Grid>
         </Box>
         <Box sx={boxStyleRow("20%")}>
