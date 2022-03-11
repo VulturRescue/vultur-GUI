cebeaf636e611daa497d6324ec543090232c0e03
miunmnn <esteban.principe@utec.edu.pe>
2022-03-11 16:09:46 -0500

    grid -> box

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,74 +24,123 @@ const StyledButton = styled(Button)(({ theme }) => ({
   margin: '3px',
 }))
 
+
+
+const boxStyleCols = (height, width, inlineBlock=true) => {
+  return  {
+    width: width,
+    height: height,
+    display: inlineBlock?'inline-block': null,
+    p: 1,
+    mx: 1,
+    bgcolor: (theme) =>
+      theme.palette.mode === 'dark' ? '#101010' : 'grey.100',
+    color: (theme) =>
+      theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
+    border: '1px solid',
+    borderColor: (theme) =>
+      theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
+    borderRadius: 2,
+    fontSize: '0.875rem',
+    fontWeight: '700',
+    textAlign: 'center',
+  }
+}
+
+const boxStyleRow = (height) => {
+  return ({
+    // width: '25%',
+    height: height,
+    p: 1,
+    bgcolor: (theme) =>
+      theme.palette.mode === 'dark' ? '#101010' : 'grey.100',
+    color: (theme) =>
+      theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
+    border: '1px solid',
+    borderColor: (theme) =>
+      theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
+    borderRadius: 2,
+    fontSize: '0.875rem',
+    fontWeight: '700',
+    textAlign: 'center',
+  })
+}
+
 const Home = () => {
   return (
-    <div
-    // style={{ backgroundColor: "black", height:'100%'  }}
+    <Box sx={{ height: '50rem', width: '100%' }}>
+      <Box sx={boxStyleCols("100%", "45%")}>
+        {/* <img src={Histograma}/> */}
+        Columna1
+      </Box>
+      <Box sx={boxStyleCols("100%", "45%")}>
+        {/* <Box sx={boxStyleRow("40%")}>
+          Row1
+        </Box>
+        <Box sx={boxStyleRow("20%")}>
+        Row2
+        </Box>
+        <Box sx={boxStyleRow("20%")}>
+        Row3
+        </Box> */}
+      </Box>
+    </Box>
+    // <div >
+    //   <Grid container spacing={2} style={{maxHeight:"24rem"}}>
+    //     <Grid item style={{minHeight: "100%"}}  xs={6}>
+    //       <Item>
+    //       <img src={Histograma}/>
+    //       </Item>
+    //     </Grid>
+    //     <Grid item style={{ "height": "100%" }} xs={6}>
+    //       <Stack
+    //         direction="column"
+    //         spacing={2}>
+    //         <Item>
+    //           <Grid container spacing={2}>
+    //             <Grid item xs={6}>
+    //               <Item
+    //                 sx={{
+    //                   backgroundColor: '#b4e8fa',
+    //                 }}
+    //               >
+    //                 <h1>
+    //                   Holaa
+    //                 </h1>
+    //               </Item>
+    //             </Grid>
 
-    >
-      <Grid container spacing={2}>
+    //             <Grid item xs={6}>
+    //               <Item>
 
-        <Grid item xs={6}>
-          <Item></Item>
-        </Grid>
-
-        <Grid item style={{ "height": "100%" }} xs={6}>
-
-          <Stack
-            direction="column"
-            spacing={2}>
-
-            <Item>
-              <Grid container spacing={2}>
-
-                <Grid item xs={6}>
-                  <Item
-                    sx={{
-                      backgroundColor: '#b4e8fa',
-                    }}
-                  >
-                    <h1>
-                      Holaa
-                    </h1>
-                  </Item>
-                </Grid>
-
-                <Grid item xs={6}>
-                  <Item>
-
-                  </Item>
-                </Grid>
-              </Grid>
-              <Grid container justifyContent="flexStart" xs={12}>
-                <StyledButton variant='contained' color='success'>ON</StyledButton>
-                <StyledButton variant='contained' color='error'>OFF</StyledButton>
-              </Grid>
-            </Item>
-
-            <Item>
-
-            </Item>
-
-            <Item></Item>
-          </Stack>
-          {/* <Grid item xs={12}>
-            <Item><img src={Histograma} alt="" /></Item>
-          </Grid>
-          <Grid item xs={12}>
-            <Item><img src={RVIZ} alt="rviz" /></Item>
-          </Grid>
-          <Grid item xs={12}>
-            <Item><img src={Histograma} alt="rviz" /></Item>
-          </Grid> */}
-
-        </Grid>
-
-        {/* <Grid item xs={6}>
-          <Item><img src={Spectogram} alt="spectograma" /></Item>
-        </Grid> */}
-      </Grid>
-    </div >
+    //               </Item>
+    //             </Grid>
+    //           </Grid>
+    //           <Grid container justifyContent="flexStart" xs={12}>
+    //             <StyledButton variant='contained' color='success'>ON</StyledButton>
+    //             <StyledButton variant='contained' color='error'>OFF</StyledButton>
+    //           </Grid>
+    //         </Item>
+    //         <Item style={{ height: "25%"}}>
+    //           <img style={{ maxWidth: "100%"}} src={Spectogram}/>
+    //         </Item>
+    //         <Item>
+    //           <Grid container spacing={1}>
+    //             <Grid item xs={4}>
+    //                 Item1
+    //             </Grid>
+    //             <Grid item xs={4}>
+    //               Item2
+    //             </Grid>
+    //             <Grid item xs={4}>
+    //               Item3
+    //             </Grid>
+    //           </Grid>
+    //         </Item>
+    //       </Stack>
+    //     </Grid>
+    //   </Grid>
+    // </div >
   )
 }
 
