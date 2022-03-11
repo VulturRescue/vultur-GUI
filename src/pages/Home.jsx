9231b849716a5e864a2ee783520d53c66178e4cc
miunmnn <esteban.principe@utec.edu.pe>
2022-03-11 16:44:08 -0500

    Indicators

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,6 @@
+
+
+import Indicators from "../components/Indicators"
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
@@ -85,7 +88,7 @@ const Home = () => {
 
         </Box>
         <Box sx={boxStyleRow("20%")}>
-          Row3
+          <Indicators/>
         </Box>
       </Box>
     </Box>
